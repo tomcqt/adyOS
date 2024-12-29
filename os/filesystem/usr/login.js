de408e2baf97f4ea92214e973e2e49d9c0a7dd52
tomcat with a q <hi@tomcat.sh>
2024-12-29 11:21:20 -0500

    write more of the login

--- os/filesystem/usr/login.js
+++ os/filesystem/usr/login.js
@@ -8,15 +8,29 @@ async function show() {
 
   let domain; // set server domain
   while (1 == 1) {
-    let ask = await wfi.asks("Server to connect to (URL): ");
+    let ask = await wfi.asks("Server to connect to: ");
     if (ask == "2") {
-      domain = await wfi.asks("Server URL: ");
+      domain = await wfi.asks("Server URL: "); // add server checking here eventually to see if its a real server. probably add something to the server to say its an adyos server too, as that would be useful.
       break;
     } else if (ask == "1") {
       domain = "adyos.tomcat.sh";
       break;
     } else {
       console.log("Try again.");
+    }
+  }
+
+  while (1 == 1) {
+    console.log("\n1. Log In\n2. Sign up\n");
+    let ask = await wfi.asks("1 or 2: ");
+    if (ask == "1") {
+      console.log("log in screen");
+      break;
+    } else if (ask == "2") {
+      console.log("sign up screen");
+      break;
+    } else {
+      console.log("Try again!");
     }
   }
 
