d4c805896aa2ac434cb1148bd3900891b54fe6d6
tomcat with a q <hi@tomcat.sh>
2025-02-04 16:47:59 +0000

    Update README.md & lines.sh

--- lines.sh
+++ lines.sh
@@ -1,1 +1,3 @@
-git ls-files | xargs wc -l | sort -n
+# awesome lines.sh file generates the thing like uh
+# Contains #.#k+ lines of code!
+echo "Contains $(echo $(( ( $(git ls-files | xargs wc -l | sort -n | cut -d' ' -f2 | grep "\S") + 500 / 500 ) / 500 * 500 )) / 1000 | bc -l | sed '/\./{s/0\+$//;s/\.$/.0/;b};s/$/.0/')k+ lines of code!"
