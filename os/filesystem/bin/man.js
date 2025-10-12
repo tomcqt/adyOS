a0540c0f90bd5b7260e2f857d23e4a10726234f7
tomcqt <hi@tomcat.sh>
2025-10-11 20:04:18 -0400

    Create manual command. (os/filesystem/bin/man.js)

--- os/filesystem/bin/man.js
+++ os/filesystem/bin/man.js
@@ -8,11 +8,32 @@ async function man(arg) {
     console.log(
       `${ezout.colours.bold}adyOS Manual (version ${version})${ezout.colours.reset}`
     );
-    console.log(`${commands.length} commands loaded!`);
+    console.log(`${Object.keys(commands).length} commands loaded!`);
     console.log(`Use \`manual manual\` for more information.`);
     return 0; // exit
   } else if (arg.cmds.length == 2) {
-    console.log("aughhhhhhh");
+    const commandName = arg.cmds[1];
+    if (commands.hasOwnProperty(commandName)) {
+      console.log(
+        `\n${ezout.colours.bold}adyOS Manual - ${ezout.colours.cyan}${commandName}${ezout.colours.reset}`
+      );
+      console.log(
+        `Aliases: ${ezout.colours.bold}${
+          Array.isArray(commands[commandName].name)
+            ? commands[commandName].name.join(
+                `${ezout.colours.reset}, ${ezout.colours.bold}`
+              )
+            : commands[commandName].name
+        }${ezout.colours.reset}`
+      );
+      console.log(
+        `Usage: ${ezout.colours.bold}${commands[commandName].usage}${ezout.colours.reset}\n`
+      );
+      console.log(`\n${commands[commandName].desc}`);
+    } else {
+      ezout.warn_nodebug("Command not found!");
+      return 0;
+    }
     return 0;
   } else {
     ezout.error_nodebug("Invalid syntax!");
